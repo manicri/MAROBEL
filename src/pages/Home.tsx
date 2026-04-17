dfbc1b28b960f22f299ed3d181c9f89f6b22fd02
manicri <crisdelrobbys@gmail.com>
2026-04-17 00:41:39 -0500

    fix: corrige ruta de import de ReservationForm

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Services from "../components/Services";
 import InstagramGallery from "../components/InstagramGallery";
-import ReservationForm from "../components/ReservationForm";
+import ReservationForm from "@/components/ReservationForm";
 import { PublicPromotions } from "../components/PublicPromotions";
 
 export default function Home() {
