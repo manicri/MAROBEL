9c7154e295693d6f67a75aff5680c7f37f8029f6
manicri <crisdelrobbys@gmail.com>
2026-06-01 17:08:08 -0500

    refactor: remove embedded reservation form from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Services from "../components/Services";
 import InstagramGallery from "../components/InstagramGallery";
-import ReservationForm from "../components/ReservationForm";
 import { PublicPromotions } from "../components/PublicPromotions";
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
       <About />
       <Services />
       <InstagramGallery />
-      <ReservationForm />
     </main>
   );
 }
