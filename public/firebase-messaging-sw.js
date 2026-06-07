f29a01c45f257c15f998d69694f4a055e7010b91
manicri <crisdelrobbys@gmail.com>
2026-06-07 18:30:32 -0500

    Agregar service worker para recibir notificaciones push

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/12.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBu-k3YHUZK91WhvK0Qzs3wKsMWMJsfdXo",
+  authDomain: "gen-lang-client-0393494087.firebaseapp.com",
+  projectId: "gen-lang-client-0393494087",
+  storageBucket: "gen-lang-client-0393494087.firebasestorage.app",
+  messagingSenderId: "492559907713",
+  appId: "1:492559907713:web:c6df89ae9396ca54fd4680",
+});
+
+firebase.messaging();
