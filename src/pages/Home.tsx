5b23fa2166f9ced3547d7579fef4c42e18b3c19e
manicri <crisdelrobbys@gmail.com>
2026-06-07 12:49:41 -0500

    Elimina promociones duplicadas del inicio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,12 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Services from "../components/Services";
 import InstagramGallery from "../components/InstagramGallery";
-import { PublicPromotions } from "../components/PublicPromotions";
 import ReservationForm from "@/components/ReservationForm";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <PublicPromotions />
       <About />
       <InstagramGallery />
       <Services />
