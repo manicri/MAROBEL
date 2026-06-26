7c7876618812430700af05c2bd981e7c9c9c02bd
manicri <crisdelrobbys@gmail.com>
2026-06-26 17:52:50 -0500

    Configurar persistencia de sesion en Supabase

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://urrbofvaftsfeiasrceo.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycmJvZnZhZnRzZmVpYXNyY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MzYyMzksImV4cCI6MjA5MTUxMjIzOX0.1q2IikxXCH7X95U93GKGj8WI-7pZPyMFLtwBLxnyhiI';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6InVycmJvZnZhZnRzZmVpYXNyY2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MzYyMzksImV4cCI6MjA5MTUxMjIzOX0.1q2IikxXCH7X95U93GKGj8WI-7pZPyMFLtwBLxnyhiI';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    persistSession: true,
+  },
+});
