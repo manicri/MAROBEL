70397aaf9e0babaf08e5067115d3c16303f3851c
manicri <crisdelrobbys@gmail.com>
2026-06-28 16:53:20 -0500

    Activar notificaciones al aceptar citas

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/AuthContext";
 import { AdminDashboard } from "./AdminDashboard";
+import AdminNotificationBridge from "./AdminNotificationBridge";
 import AdminOverview from "./AdminOverview";
 import ScheduleAdminDashboard from "./ScheduleAdminDashboard";
 
@@ -7,6 +8,7 @@ export default function AdminPanel() {
   const { canManageServices } = useAuth();
 
   return <>
+    <AdminNotificationBridge />
     <AdminOverview />
     {canManageServices ? <AdminDashboard /> : <ScheduleAdminDashboard />}
   </>;
